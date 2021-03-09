834501ba1b1759f6a3f68d7c2658c97c82bd9a2d
ZenPtc <maku4fb@gmail.com>
2021-03-09 15:03:59 +0700

    1-3

--- index.ts
+++ index.ts
@@ -17,14 +17,35 @@ interface JWTPayload {
   password: string;
 }
 
+interface User{
+  username:string,
+  password:string,
+  firstname:string,
+  lastname:string,
+  balance:number
+}
+const users:User[] = [];
+
 app.post('/login',
   (req, res) => {
 
     const { username, password } = req.body
     // Use username and password to create token.
-
+    const token = jwt.sign({username,password},SECRET)
+    const AEuser = users.find(user => user.username === username)
+    if(!AEuser){
+      return res.status(400).json({
+        message: 'Invalid username or password'
+      })
+    }
+    if(AEuser.password !== password){
+      return res.status(400).json({
+        message: 'Invalid username or password'
+      })
+    }
     return res.status(200).json({
       message: 'Login succesfully',
+      token: token
     })
   })
 
@@ -32,6 +53,23 @@ app.post('/register',
   (req, res) => {
 
     const { username, password, firstname, lastname, balance } = req.body
+    const user = {username, password, firstname, lastname, balance}
+    //already exist
+    const AEuser = users.find(user => user.username === username)
+    if(AEuser){
+      res.status(400)
+      res.json({
+      message: "Username is already in used"
+    })
+    return
+    }
+    //pass
+    users.push(user)
+    res.status(200)
+    res.json({
+      message: "Register successfully"
+    })
+    return
   })
 
 app.get('/balance',
@@ -39,10 +77,20 @@ app.get('/balance',
     const token = req.query.token as string
     try {
       const { username } = jwt.verify(token, SECRET) as JWTPayload
-  
+      const AEuser = users.find(user => user.username === username)
+      res.status(200)
+      res.json({
+        "name": username,
+        "balance": AEuser?.balance
+      })
     }
     catch (e) {
       //response in case of invalid token
+      res.status(401)
+      res.json({
+          message: "Invalid token"
+      })
+      return
     }
   })
 
